474a94203a10b5fbb1546d7711a0f3ebe6372ac3
wclansar <ansar.husain@williamscommerce.com>
2019-04-08 13:02:06 +0530

    admin username

---
+++ Order/CommentsUser/registration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Ansar Husain
+ * Order_CommentsUser
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+  \Magento\Framework\Component\ComponentRegistrar::MODULE,
+  'Order_CommentsUser',
+  __DIR__
+);
